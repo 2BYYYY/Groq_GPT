73f95e3ef30bd118e8892f1eb9e7ba68ab6b3fee
Toby Javelona <tobyyjavelona@gmail.com>
2025-11-10 19:08:54 +0800

    chore: prints the collection

--- check_volume.py
+++ check_volume.py
@@ -4,3 +4,4 @@ volume_path = "/usls_scholarships"
 client = chromadb.PersistentClient(path=volume_path)
 collection = client.get_or_create_collection(name="USLS_SCHOLARSHIPS")
 print("Number of entries:", collection.count())
+print(collection.get())
